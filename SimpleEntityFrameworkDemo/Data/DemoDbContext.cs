1302edceb56f5f6e4e10adee94e722c667b24e81
Mohamed Seada <mohamed.seada.1994@gmail.com>
2021-10-24 18:07:06 +0200

    upgrade to net6

--- SimpleEntityFrameworkDemo/Data/DemoDbContext.cs
+++ SimpleEntityFrameworkDemo/Data/DemoDbContext.cs
@@ -26,31 +26,5 @@ namespace SimpleEntityFrameworkDemo.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
-
-        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
-        {
-            var changedEntities = ChangeTracker.Entries()
-                .Where(e => e.State is EntityState.Added or EntityState.Modified or EntityState.Deleted)
-                .ToList();
-
-
-            //var IsKeyUnknown = (typeof(EntityEntry).GetProperty("InternalEntry", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(changedEntities[0]) as InternalEntityEntry).IsKeyUnknown;
-            //Console.WriteLine($"IsKeyUnknown: {IsKeyUnknown}");
-
-            var addedEntities = ChangeTracker.Entries()
-                .Where(e => e.State is EntityState.Added);
-
-            foreach (var entity in addedEntities)
-            {
-                var tenantIdProperty = entity.Property("TenantId");
-                tenantIdProperty.CurrentValue = tenantIdProperty.CurrentValue;
-            }
-
-
-            //IsKeyUnknown = (typeof(EntityEntry).GetProperty("InternalEntry", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(changedEntities[0]) as InternalEntityEntry).IsKeyUnknown;
-            //Console.WriteLine($"IsKeyUnknown: {IsKeyUnknown}");
-
-            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-        }
     }
 }
--- SimpleEntityFrameworkDemo/Data/TenantIdGenerator.cs
+++ SimpleEntityFrameworkDemo/Data/TenantIdGenerator.cs
@@ -10,5 +10,6 @@ namespace SimpleEntityFrameworkDemo.Data
             => Guid.Parse("98D06A82-C691-4988-EA39-08D98E2C8D8F");
 
         public override bool GeneratesTemporaryValues => false;
+        public override bool GeneratesStableValues => true;
     }
 }
